1938a59ef60ae571598e01f43575887c945f43f8
Ana Claudia <89166056+Anaclopes@users.noreply.github.com>
2024-09-06 19:39:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,80 @@
+let dados = [
+   { 
+    titulo: "Tarsila do Amaral",
+    descricao: "Tarsila do Amaral foi uma das mais importantes artistas brasileiras, sendo um dos pilares do Modernismo no Brasil. Suas obras, marcadas por cores vibrantes e temas que retratam a identidade brasileira, como a natureza e o povo, a tornaram um ícone da arte nacional. Pioneira em buscar uma expressão artística genuinamente brasileira, Tarsila influenciou gerações de artistas e continua sendo uma referência na arte brasileira até hoje.",
+    link: "https://www.tarsiladoamaral.com.br/",
+    tags: "modernismo modernista pioneira identidade"
+   },
+   {
+    titulo: "Oswald de Andrade",
+    descricao: "Oswald de Andrade foi um dos principais nomes da Semana de Arte Moderna de 1922 e uma figura central do Modernismo brasileiro. Poeta, romancista e dramaturgo, suas obras, como Memórias Sentimentais de João Miramar e Pau-Brasil, marcaram uma nova fase da literatura brasileira, valorizando a cultura nacional e a língua portuguesa. Suas ideias sobre a poesia concreta e a antropofagia cultural influenciaram gerações de artistas e intelectuais.",
+    link: "https://www.companhiadasletras.com.br/colaborador/02226/oswald-de-andrade?srsltid=AfmBOoqst20WiNCtAs_xa0kpAgi3-f1Bg1KKeFZ69oyxLR2D5Slyy4dE",
+    tags: "modernismo modernista romance romancista dramaturgia dramaturgo"
+   },
+   {
+    titulo: "Guilherme de Almeida",
+    descricao: "Poeta e crítico literário, Guilherme de Almeida foi um dos principais nomes da poesia parnasiana no Brasil. Posteriormente, adotou um estilo mais moderno, explorando temas nacionais e experimentando novas formas poéticas.",
+    link: "https://www.casaguilhermedealmeida.org.br/casa-guilherme-de-almeida/",
+    tags: "poeta critico crítico critico literario crítico literário parnasiano parnasianismo"
+   },
+   {
+    titulo: "Menotti del Picchia",
+    descricao: "Poeta, jornalista e crítico literário, Menotti del Picchia foi um dos organizadores da Semana de Arte Moderna. Sua obra poética aborda temas sociais e políticos, com um estilo marcado pela crítica e pelo engajamento.",
+    link: "https://www.academia.org.br/academicos/menotti-del-picchia/biografia",
+    tags: "jornalista critico crítico picchia"
+   },
+   {
+    titulo: "Graça Aranha",
+    descricao: "Escritor e diplomata, Graça Aranha é conhecido por seu romance Canaã, considerado uma das obras fundadoras do Modernismo brasileiro. Sua obra explora temas como a identidade nacional e a mestiçagem.",
+    link: "https://www.todamateria.com.br/graca-aranha/",
+    tags: "canaa canaã mestiço mestico aranha"
+   },
+   {
+    titulo: "Mário de Andrade",
+    descricao: "Um dos maiores nomes da cultura brasileira, Mário de Andrade foi poeta, músico, crítico literário e folclorista. Sua obra, marcada pela valorização da cultura popular brasileira, exerceu grande influência sobre as artes e a literatura do país.",
+    link: "https://www.ebiografia.com/mario_andrade/",
+    tags: "musico músico folclorista folclore"
+   },
+   {
+    titulo: "Ronald de Carvalho",
+    descricao: "Poeta e crítico literário, Ronald de Carvalho foi um dos participantes da Semana de Arte Moderna. Sua obra poética aborda temas como a natureza, a religiosidade e a condição humana.",
+    link: "https://memoriaglobo.globo.com/perfil/ronald-de-carvalho/noticia/ronald-de-carvalho.ghtml",
+    tags: "natureza religião religioso"
+   },
+   {
+    titulo: "Heitor Villa-Lobos",
+    descricao: "Compositor e maestro, Heitor Villa-Lobos é considerado um dos maiores nomes da música brasileira. Suas obras, inspiradas na música popular brasileira e na natureza, são reconhecidas internacionalmente.",
+    link: "https://www.ebiografia.com/heitor_villa_lobos/",
+    tags: "compositor maestro"
+   },
+   {
+    titulo: "Victor Brecheret",
+    descricao: "Escultor italiano naturalizado brasileiro, Victor Brecheret é conhecido por suas obras de grande porte e realismo. Suas esculturas, como o Monumento às Bandeiras, são marcos da arte brasileira.",
+    link: "https://www.todamateria.com.br/victor-brecheret/",
+    tags: "escultor realista realismo"
+   },
+   {
+    titulo: "Anita Malfatti",
+    descricao: "Pintora, desenhista, gravadora e professora, Anita Malfatti foi uma das principais figuras da arte moderna brasileira. Suas obras, marcadas por um estilo expressionista, causaram grande polêmica na época.",
+    link: "https://www.ebiografia.com/anita_malfatti/",
+    tags: "pintora desenhista gravadora professora"
+   },
+   {
+    titulo: "Di Cavalcanti",
+    descricao: "Pintor, desenhista e ilustrador, Di Cavalcanti foi um dos principais nomes do Modernismo brasileiro. Suas obras, marcadas por um forte colorido e pela representação do povo brasileiro, são um marco da arte nacional.",
+    link: "https://www.ebiografia.com/di_cavalcanti/",
+    tags: "ilustrador desenhista colorido cavalcanti"
+   },
+   {
+    titulo: "Zina Aita",
+    descricao: "Pintora, desenhista e gravadora, Zina Aita foi uma das primeiras mulheres a se destacar na arte moderna brasileira. Suas obras, marcadas por um estilo expressionista e pela representação de temas sociais, são um importante testemunho de seu tempo.",
+    link: "https://www.guiadasartes.com.br/zina-aita/obras-e-biografia",
+    tags: "pintora desenhista gravadora"
+   },
+   {
+    titulo: "Vicente do Rego Monteiro",
+    descricao: "Pintor, desenhista e ilustrador, Vicente do Rego Monteiro foi um dos principais nomes da arte moderna brasileira. Suas obras, marcadas por um estilo sintético e pela representação de paisagens e figuras humanas, são um exemplo da diversidade da arte brasileira.",
+    link: "https://www.escritoriodearte.com/artista/vicente-do-rego-monteiro",
+    tags: "ilustrados desenhista paisagens paisagista"
+   }
+];
